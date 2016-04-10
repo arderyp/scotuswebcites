8b85d11e6f7249e54f4e6399147d3ea1cfb32081
Philip Ardery <arderyp@gmail.com>
2016-04-10 11:44:39 -0700

    Adding simply daily database backup script

---
+++ scripts/dailybackup.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Call this in your root's crontab to run once a day on weekdays
+# Example call: /path/to/scotuswebcites/scripts/dailybackup.sh /path/to/backup/directory
+
+function getSetting {
+    if [[ -f "$SETTINGS_FILE" ]]; then
+        grep "'$1':" "$SETTINGS_FILE" | awk '{print $2}' | sed "s/',$//g" | sed "s/'$//g" | sed "s/^'//g"
+    fi
+}
+
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run as root"
+    exit 1
+fi
+
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: ./dailybackup.sh /path/to/backup/dir"
+    exit 1
+fi
+
+DAY=$(date +%A)
+BACKUP_DIR="$1"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SETTINGS_FILE="$SCRIPT_DIR"/../scotuswebcites/settings.py
+DATABASE_NAME=$(getSetting 'NAME')
+DATABASE_USER=$(getSetting 'USER')
+DATABASE_PASSWORD=$(getSetting 'PASSWORD')
+DATABASE_HOST=$(getSetting 'HOST')
+DATABASE_PORT=$(getSetting 'PORT')
+
+if [[ -n "$DATABASE_USER" && -n "$DATABASE_PASSWORD" && -n "$DATABASE_HOST" && -n "$DATABASE_PORT" && -n "$DATABASE_NAME" ]]; then
+
+    if [[ ! -d "$BACKUP_DIR" ]]; then
+        mkdir "$BACKUP_DIR"
+    fi
+
+    OUTPUT="$BACKUP_DIR/$DAY.sql.gz"
+    mysqldump -u "$DATABASE_USER" -p"$DATABASE_PASSWORD" -h "$DATABASE_HOST" -P "$DATABASE_PORT" "$DATABASE_NAME" | gzip -c | cat > "$OUTPUT"
+    chmod 600 "$OUTPUT"
+else
+    echo "ERROR: Could not establish database variables from settings.py file."
+fi
+
