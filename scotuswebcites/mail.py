585fa2478e826c9f3d010ff352bf38793c4536e6
Philip Ardery <arderyp@protonmail.com>
2020-06-18 09:23:01 -0700

    simplifying mailer function

--- scotuswebcites/mail.py
+++ scotuswebcites/mail.py
@@ -5,14 +5,12 @@ from django.core.mail import EmailMultiAlternatives
 def send_email(subject: str, body: str, recipient: str):
     if 'YOUR_GMAIL_ADDRESS' in [settings.SENDER_EMAIL, settings.EMAIL_HOST_USER]:
         raise Exception("Email setting not configured")
-    sender = settings.SENDER_EMAIL
-    bcc = [settings.SENDER_EMAIL]
     email = EmailMultiAlternatives(
         subject=subject,
         body=body,
-        from_email=sender,
+        from_email=settings.SENDER_EMAIL,
         to=[recipient],
-        bcc=bcc
+        bcc=[settings.SENDER_EMAIL]
     )
     email.attach_alternative(body, "text/html")
     email.send()
