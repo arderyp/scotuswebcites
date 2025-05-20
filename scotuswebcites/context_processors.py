6511e44e06ea2551c39b0532ddf0a837b967686c
Philip Ardery <arderyp@protonmail.com>
2025-05-20 08:36:07 -0700

    adding support for SiteImprove script

--- scotuswebcites/context_processors.py
+++ scotuswebcites/context_processors.py
@@ -1,2 +1,7 @@
+from scotuswebcites import settings
+
 def global_variables(request):
-    return {'VERSION': '4.0.17'}
+    return {
+        'SITE_IMPROVE_SCRIPT_URL': settings.SITE_IMPROVE_SCRIPT_URL,
+        'VERSION': '4.0.17'
+    }
