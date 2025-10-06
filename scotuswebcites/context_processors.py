8ea2e1c3713ea050fdbfecd2554b0412f7904d37
Philip Ardery <arderyp@protonmail.com>
2025-10-06 15:22:22 -0700

    version bump minor

--- scotuswebcites/context_processors.py
+++ scotuswebcites/context_processors.py
@@ -3,5 +3,5 @@ from scotuswebcites import settings
 def global_variables(request):
     return {
         'SITE_IMPROVE_SCRIPT_URL': settings.SITE_IMPROVE_SCRIPT_URL,
-        'VERSION': '4.1.4'
+        'VERSION': '4.1.5'
     }
