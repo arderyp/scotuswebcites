bfacdd0e455d7a45e90f72e063a67dc194554911
Philip Ardery <arderyp@protonmail.com>
2026-01-05 14:05:55 -0800

    rough stab at removing bootstrap and jquery in favor of pico.css. upgrading HighCharts to latest

--- static/js/data.js
+++ static/js/data.js
@@ -3,13 +3,6 @@
  * @author Torstein Honsi
  */
 
-// Load the fonts
-Highcharts.createElement('link', {
-	href: '//fonts.googleapis.com/css?family=Dosis:400,600',
-	rel: 'stylesheet',
-	type: 'text/css'
-}, null, document.getElementsByTagName('head')[0]);
-
 Highcharts.theme = {
 	colors: ["#7cb5ec", "#f7a35c", "#90ee7e", "#7798BF", "#aaeeee", "#ff0066", "#eeaaee",
 		"#55BF3B", "#DF5353", "#7798BF", "#aaeeee"],
